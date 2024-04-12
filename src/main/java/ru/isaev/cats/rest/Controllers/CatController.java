7ebceb1b0f1e822f6fbb8a5b94a6ac6aadcabe01
theSirD <isaev-daniel17@yandex.ru>
2024-04-12 19:25:21 +0300

    feat: created CatController

---
+++ src/main/java/ru/isaev/cats/rest/Controllers/CatController.java
@@ -0,0 +1,50 @@
+package ru.isaev.cats.rest.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.isaev.cats.rest.Entities.CatDtos.CatDto;
+import ru.isaev.cats.rest.Entities.Cats.Cat;
+import ru.isaev.cats.rest.Entities.Mapper.IMyMapper;
+import ru.isaev.cats.rest.Service.CatService;
+
+import java.util.List;
+
+@RestController
+public class CatController {
+    private final CatService catService;
+
+    private final IMyMapper mapper;
+
+    @Autowired
+    public CatController(CatService catService, IMyMapper mapper) {
+        this.catService = catService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/cats")
+    public List<Cat> getAll() {
+        return catService.getAllCats();
+    }
+
+//    @GetMapping("/cats/{id}")
+//    public Cat getCatById(@PathVariable Long id) {
+//        return catService.getCatById(id).get();
+//    }
+
+    @GetMapping("/cats/{id}")
+    public ResponseEntity<CatDto> getCatById(@PathVariable Long id) {
+        return new ResponseEntity<>(
+                mapper.catToCatDto(catService.getCatById(id).get()),
+                HttpStatus.OK
+        );
+    }
+}

a7e1c3e4b24bf8dcc8e744d16e8597a33839c5f5
theSirD <isaev-daniel17@yandex.ru>
2024-04-12 19:24:30 +0300

    feat: updated Service classes to use methods implemented by Spring for JpaRepository

--- src/main/java/ru/isaev/cats/rest/Service/CatService.java
+++ src/main/java/ru/isaev/cats/rest/Service/CatService.java
@@ -1,34 +1,44 @@
 package ru.isaev.cats.rest.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.isaev.cats.rest.DAO.ICatDAO;
 import ru.isaev.cats.rest.Entities.Cats.Cat;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CatService {
     private final ICatDAO catDAO;
 
+    @Autowired
     public CatService(ICatDAO catDAO) {
         this.catDAO = catDAO;
     }
 
     public void addCat(Cat cat) {
-        catDAO.add(cat);
+//        catDAO.add(cat);
+        catDAO.save(cat);
     }
 
     public List<Cat> getAllCats() {
-        return catDAO.getAll();
+//        return catDAO.getAll();
+        return catDAO.findAll();
     }
 
-    public Cat getCatById(Long id) {
-        return catDAO.getById(id);
+    public Optional<Cat> getCatById(Long id) {
+//        return catDAO.getById(id);
+        return catDAO.findById(id);
     }
 
     public void updateCat(Cat cat) {
-        catDAO.update(cat);
+//        catDAO.update(cat);
+        catDAO.save(cat);
     }
 
     public void removeCat(Cat cat) {
-        catDAO.remove(cat);
+//        catDAO.remove(cat);
+        catDAO.delete(cat);
     }
 }
--- src/main/java/ru/isaev/cats/rest/Service/OwnerService.java
+++ src/main/java/ru/isaev/cats/rest/Service/OwnerService.java
@@ -1,34 +1,44 @@
 package ru.isaev.cats.rest.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.isaev.cats.rest.Entities.Owners.Owner;
 import ru.isaev.cats.rest.DAO.IOwnerDAO;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class OwnerService {
-    private final IOwnerDAO ownerDAO;
 
+    private final IOwnerDAO ownerDAO;
+    @Autowired
     public OwnerService(IOwnerDAO ownerDAO) {
         this.ownerDAO = ownerDAO;
     }
 
     public void addOwner(Owner owner) {
-        ownerDAO.add(owner);
+//        ownerDAO.add(owner);
+        ownerDAO.save(owner);
     }
 
     public List<Owner> getAllOwners() {
-        return ownerDAO.getAll();
+//        return ownerDAO.getAll();
+        return ownerDAO.findAll();
     }
 
-    public Owner getOwnerById(Long id) {
-        return ownerDAO.getById(id);
+    public Optional<Owner> getOwnerById(Long id) {
+        return ownerDAO.findById(id);
+
     }
 
     public void updateOwner(Owner owner) {
-        ownerDAO.update(owner);
+//        ownerDAO.update(owner);
+        ownerDAO.save(owner);
     }
 
     public void removeOwner(Owner owner) {
-        ownerDAO.remove(owner);
+//        ownerDAO.remove(owner);
+        ownerDAO.delete(owner);
     }
 }

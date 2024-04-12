5e08407b2f96e5742c8cddbe877dedf7d30e13c3
theSirD <isaev-daniel17@yandex.ru>
2024-04-12 19:23:47 +0300

    feat: extended JpaRepository in DAO interfaces

--- src/main/java/ru/isaev/cats/rest/DAO/ICatDAO.java
+++ src/main/java/ru/isaev/cats/rest/DAO/ICatDAO.java
@@ -1,17 +1,18 @@
 package ru.isaev.cats.rest.DAO;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.isaev.cats.rest.Entities.Cats.Cat;
 
 import java.util.List;
 
-public interface ICatDAO {
-    void add(Cat cat);
-
-    List<Cat> getAll();
-
-    Cat getById(Long Id);
-
-    void update(Cat cat);
-
-    void remove(Cat cat);
+public interface ICatDAO extends JpaRepository<Cat, Long> {
+//    void add(Cat cat);
+//
+//    List<Cat> getAll();
+//
+//    Cat getById(Long Id);
+//
+//    void update(Cat cat);
+//
+//    void remove(Cat cat);
 }
--- src/main/java/ru/isaev/cats/rest/DAO/IOwnerDAO.java
+++ src/main/java/ru/isaev/cats/rest/DAO/IOwnerDAO.java
@@ -1,17 +1,18 @@
 package ru.isaev.cats.rest.DAO;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.isaev.cats.rest.Entities.Owners.Owner;
 
 import java.util.List;
 
-public interface IOwnerDAO {
-    void add(Owner owner);
-
-    List<Owner> getAll();
-
-    Owner getById(Long id);
-
-    void update(Owner owner);
-
-    void remove(Owner owner);
+public interface IOwnerDAO extends JpaRepository<Owner, Long> {
+//    void add(Owner owner);
+//
+//    List<Owner> getAll();
+//
+//    Owner getById(Long id);
+//
+//    void update(Owner owner);
+//
+//    void remove(Owner owner);
 }
